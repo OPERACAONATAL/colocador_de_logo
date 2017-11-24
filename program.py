2a7d52b7267c0621e71b21d1f1a6f8e71dd74812
farm <lukas_karotta@hotmail.com>
2017-11-24 11:43:10 -0200

    Arrumando código para imagens de tamanhos diferentes.

--- program.py
+++ program.py
@@ -5,19 +5,28 @@ from tqdm import tqdm
 
 # o menos um é para carregar a imagem com transparencia
 logo = cv2.imread('./src/fundo.png', -1)
-offset = 10
 
 def insertLogo(img, logo, id):
-    x_offset = y_offset = 10
-    y1, y2 = y_offset, y_offset + logo.shape[0]
-    x1, x2 = x_offset, x_offset + logo.shape[1]
+    #   Redimenciona o logo para ficar proporcional a imagem
+    ratio = (img.shape[0] / img.shape[1])
+    #   Já viu, né?
+    if ratio > 1.0:
+        ratio = ratio * 0.6
+    logo = cv2.resize(logo, (0, 0), fx=ratio, fy=ratio)
+
+    #   Caso mesmo após o redimencionamento o logo ainda fique maior que a imagem, redimensionar novamente.
+    while img.shape[0] < logo.shape[0] or img.shape[1] < logo.shape[1]:
+        logo = cv2.resize(logo, (0, 0), fx=ratio, fy=ratio)
+    
+    offset = 10
+    x1, x2 = offset, offset + logo.shape[0]
+    y1, y2 = offset, offset + logo.shape[1]
 
     alpha_s = logo[:, :, 3] / 255.0
     alpha_l = 1.0 - alpha_s
 
     for c in range(0, 3):
-        img[y1:y2, x1:x2, c] = (alpha_s * logo[:, :, c] +
-                                alpha_l * img[y1:y2, x1:x2, c])
+        img[y1:y2, x1:x2, c] = (alpha_s * logo[:, :, c] + alpha_l * img[y1:y2, x1:x2, c])
 
     # Salva a imagem com o logo
     cv2.imwrite(f"./dist/img/OPN-{id}.png", img)
